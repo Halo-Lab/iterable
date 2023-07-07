a9760b8fb6ad2fb31f6fde681f282fb665ca89ee
Yevhen Kapelianovych <kapelianovych.y.v@protonmail.com>
2023-07-07 11:29:06 +0300

    Format code and make the from function smaller.

--- index.d.ts
+++ index.d.ts
@@ -1,229 +1,229 @@
 export interface List<A> {
-  (): Iterator<A, void, unknown>
-  readonly [Symbol.iterator]: List<A>
+  (): Iterator<A, void, unknown>;
+  readonly [Symbol.iterator]: List<A>;
 }
 
-export function isList<A>(value: List<A>): true
-export function isList<A>(value: unknown): value is List<A>
+export function isList<A>(value: List<A>): true;
+export function isList<A>(value: unknown): value is List<A>;
 
 export function map<A, B>(
-  callback: (value: A) => B
-): (source: List<A>) => List<B>
-export function map<A, B>(source: List<A>, callback: (value: A) => B): List<B>
+  callback: (value: A) => B,
+): (source: List<A>) => List<B>;
+export function map<A, B>(source: List<A>, callback: (value: A) => B): List<B>;
 
 export function chain<A, B>(
-  callback: (value: A) => List<B>
-): (source: List<A>) => List<B>
+  callback: (value: A) => List<B>,
+): (source: List<A>) => List<B>;
 export function chain<A, B>(
   source: List<A>,
-  callback: (value: A) => List<B>
-): List<B>
+  callback: (value: A) => List<B>,
+): List<B>;
 
 export function filter(
-  predicate: BooleanConstructor
-): <A>(source: List<A>) => List<Exclude<A, null | undefined | 0 | '' | false>> // except NaN because it has the "number" type
+  predicate: BooleanConstructor,
+): <A>(source: List<A>) => List<Exclude<A, null | undefined | 0 | "" | false>>; // except NaN because it has the "number" type
 export function filter<A, B extends A>(
-  predicate: (value: A) => value is B
-): (source: List<A>) => List<B>
+  predicate: (value: A) => value is B,
+): (source: List<A>) => List<B>;
 export function filter<A>(
-  predicate: (value: A) => boolean
-): (source: List<A>) => List<A>
+  predicate: (value: A) => boolean,
+): (source: List<A>) => List<A>;
 export function filter<A>(
   source: List<A>,
-  predicate: BooleanConstructor
-): List<Exclude<A, null | undefined | 0 | '' | false>> // except NaN because it has the "number" type
+  predicate: BooleanConstructor,
+): List<Exclude<A, null | undefined | 0 | "" | false>>; // except NaN because it has the "number" type
 export function filter<A, B extends A>(
   source: List<A>,
-  predicate: (value: A) => value is B
-): List<B>
+  predicate: (value: A) => value is B,
+): List<B>;
 export function filter<A>(
   source: List<A>,
-  predicate: (value: A) => boolean
-): List<A>
+  predicate: (value: A) => boolean,
+): List<A>;
 
 export function forEach<A>(
-  callback: (value: A) => void
-): (source: List<A>) => void
-export function forEach<A>(source: List<A>, callback: (value: A) => void): void
+  callback: (value: A) => void,
+): (source: List<A>) => void;
+export function forEach<A>(source: List<A>, callback: (value: A) => void): void;
 
 export function of<const T extends readonly unknown[]>(
   ...values: T
-): List<T[number]>
+): List<T[number]>;
 
 export function from<const A>(
-  value: (() => Iterator<A, void, unknown>) | Iterable<A> | ArrayLike<A>
-): List<A>
+  value: (() => Iterator<A, void, unknown>) | Iterable<A> | ArrayLike<A>,
+): List<A>;
 
 export function fold<A>(
-  reducer: (accumulator: A, value: A) => A
-): (source: List<A>) => A
+  reducer: (accumulator: A, value: A) => A,
+): (source: List<A>) => A;
 export function fold<A, const B>(
   accumulator: B,
-  reducer: (accumulator: B, value: A) => B
-): (source: List<A>) => B
+  reducer: (accumulator: B, value: A) => B,
+): (source: List<A>) => B;
 export function fold<A>(
   source: List<A>,
-  reducer: (accumulator: A, value: A) => A
-): A
+  reducer: (accumulator: A, value: A) => A,
+): A;
 export function fold<A, const B>(
   source: List<A>,
   accumulator: B,
-  reducer: (accumulator: B, value: A) => B
-): B
+  reducer: (accumulator: B, value: A) => B,
+): B;
 
-export function concat<A>(list: Iterable<A>): (source: List<A>) => List<A>
-export function concat<A>(source: List<A>, list: Iterable<A>): List<A>
+export function concat<A>(list: Iterable<A>): (source: List<A>) => List<A>;
+export function concat<A>(source: List<A>, list: Iterable<A>): List<A>;
 
 export function all<A>(
-  predicate: (value: A) => boolean
-): (source: List<A>) => boolean
+  predicate: (value: A) => boolean,
+): (source: List<A>) => boolean;
 export function all<A>(
   source: List<A>,
-  predicate: (value: A) => boolean
-): boolean
+  predicate: (value: A) => boolean,
+): boolean;
 
 export function any<A>(
-  predicate: (value: A) => boolean
-): (source: List<A>) => boolean
+  predicate: (value: A) => boolean,
+): (source: List<A>) => boolean;
 export function any<A>(
   source: List<A>,
-  predicate: (value: A) => boolean
-): boolean
+  predicate: (value: A) => boolean,
+): boolean;
 
-export function take(amount: number): <A>(source: List<A>) => List<A>
-export function take<A>(source: List<A>, amount: number): List<A>
+export function take(amount: number): <A>(source: List<A>) => List<A>;
+export function take<A>(source: List<A>, amount: number): List<A>;
 
-export function skip(amount: number): <A>(source: List<A>) => List<A>
-export function skip<A>(source: List<A>, amount: number): List<A>
+export function skip(amount: number): <A>(source: List<A>) => List<A>;
+export function skip<A>(source: List<A>, amount: number): List<A>;
 
 export function takeWhile<A>(
-  predicate: (value: A) => boolean
-): (source: List<A>) => List<A>
+  predicate: (value: A) => boolean,
+): (source: List<A>) => List<A>;
 export function takeWhile<A>(
   source: List<A>,
-  predicate: (value: A) => boolean
-): List<A>
+  predicate: (value: A) => boolean,
+): List<A>;
 
 export function skipWhile<A>(
-  predicate: (value: A) => boolean
-): (source: List<A>) => List<A>
+  predicate: (value: A) => boolean,
+): (source: List<A>) => List<A>;
 export function skipWhile<A>(
   source: List<A>,
-  predicate: (value: A) => boolean
-): List<A>
+  predicate: (value: A) => boolean,
+): List<A>;
 
 export function enumerate<A>(
-  source: List<A>
-): List<readonly [value: A, index: number]>
+  source: List<A>,
+): List<readonly [value: A, index: number]>;
 
 export function sort<A>(
-  compare: (first: A, second: A) => number
-): (source: List<A>) => List<A>
+  compare: (first: A, second: A) => number,
+): (source: List<A>) => List<A>;
 export function sort<A>(
   source: List<A>,
-  compare: (first: A, second: A) => number
-): List<A>
+  compare: (first: A, second: A) => number,
+): List<A>;
 
-export function count<A>(source: List<A>): number
+export function count<A>(source: List<A>): number;
 
 export function scan<A>(
-  reducer: (accumulator: A, value: A) => A
-): (source: List<A>) => List<A>
+  reducer: (accumulator: A, value: A) => A,
+): (source: List<A>) => List<A>;
 export function scan<A, const B>(
   accumulator: B,
-  reducer: (accumulator: B, value: A) => B
-): (source: List<A>) => List<B>
+  reducer: (accumulator: B, value: A) => B,
+): (source: List<A>) => List<B>;
 export function scan<A>(
   source: List<A>,
-  reducer: (accumulator: A, value: A) => A
-): List<A>
+  reducer: (accumulator: A, value: A) => A,
+): List<A>;
 export function scan<A, const B>(
   source: List<A>,
   accumulator: B,
-  reducer: (accumulator: B, value: A) => B
-): List<B>
+  reducer: (accumulator: B, value: A) => B,
+): List<B>;
 
-export function first<A>(source: List<A>): A | undefined
+export function first<A>(source: List<A>): A | undefined;
 
-export function isEmpty<A>(source: List<A>): boolean
+export function isEmpty<A>(source: List<A>): boolean;
 
-export function last<A>(source: List<A>): A | undefined
+export function last<A>(source: List<A>): A | undefined;
 
 export function zip<B>(
-  other: List<B>
-): <A>(source: List<A>) => List<readonly [A, B]>
+  other: List<B>,
+): <A>(source: List<A>) => List<readonly [A, B]>;
 export function zip<A, B>(
   source: List<A>,
-  other: List<B>
-): List<readonly [A, B]>
+  other: List<B>,
+): List<readonly [A, B]>;
 
 export function find<A, B extends A>(
-  predicate: (value: A) => value is B
-): (source: List<A>) => B | undefined
+  predicate: (value: A) => value is B,
+): (source: List<A>) => B | undefined;
 export function find<A>(
-  predicate: (value: A) => boolean
-): (source: List<A>) => A | undefined
+  predicate: (value: A) => boolean,
+): (source: List<A>) => A | undefined;
 export function find<A, B extends A>(
   source: List<A>,
-  predicate: (value: A) => value is B
-): B | undefined
+  predicate: (value: A) => value is B,
+): B | undefined;
 export function find<A>(
   source: List<A>,
-  predicate: (value: A) => boolean
-): A | undefined
+  predicate: (value: A) => boolean,
+): A | undefined;
 
-type _of = typeof of
-type _is = typeof isList
-type _all = typeof all
-type _any = typeof any
-type _zip = typeof zip
-type _map = typeof map
-type _take = typeof take
-type _skip = typeof skip
-type _last = typeof last
-type _from = typeof from
-type _scan = typeof scan
-type _sort = typeof sort
-type _fold = typeof fold
-type _find = typeof find
-type _chain = typeof chain
-type _count = typeof count
-type _first = typeof first
-type _filter = typeof filter
-type _concat = typeof concat
-type _isEmpty = typeof isEmpty
-type _forEach = typeof forEach
-type _takeWhile = typeof takeWhile
-type _skipWhile = typeof skipWhile
-type _enumerate = typeof enumerate
+type _of = typeof of;
+type _is = typeof isList;
+type _all = typeof all;
+type _any = typeof any;
+type _zip = typeof zip;
+type _map = typeof map;
+type _take = typeof take;
+type _skip = typeof skip;
+type _last = typeof last;
+type _from = typeof from;
+type _scan = typeof scan;
+type _sort = typeof sort;
+type _fold = typeof fold;
+type _find = typeof find;
+type _chain = typeof chain;
+type _count = typeof count;
+type _first = typeof first;
+type _filter = typeof filter;
+type _concat = typeof concat;
+type _isEmpty = typeof isEmpty;
+type _forEach = typeof forEach;
+type _takeWhile = typeof takeWhile;
+type _skipWhile = typeof skipWhile;
+type _enumerate = typeof enumerate;
 
 declare namespace List {
-  export type Self<A> = List<A>
+  export type Self<A> = List<A>;
 
-  export const of: _of
-  export const is: _is
-  export const all: _all
-  export const any: _any
-  export const zip: _zip
-  export const map: _map
-  export const take: _take
-  export const sort: _sort
-  export const skip: _skip
-  export const last: _last
-  export const from: _from
-  export const scan: _scan
-  export const fold: _fold
-  export const find: _find
-  export const chain: _chain
-  export const count: _count
-  export const first: _first
-  export const filter: _filter
-  export const concat: _concat
-  export const isEmpty: _isEmpty
-  export const forEach: _forEach
-  export const takeWhile: _takeWhile
-  export const skipWhile: _skipWhile
-  export const enumerate: _enumerate
+  export const of: _of;
+  export const is: _is;
+  export const all: _all;
+  export const any: _any;
+  export const zip: _zip;
+  export const map: _map;
+  export const take: _take;
+  export const sort: _sort;
+  export const skip: _skip;
+  export const last: _last;
+  export const from: _from;
+  export const scan: _scan;
+  export const fold: _fold;
+  export const find: _find;
+  export const chain: _chain;
+  export const count: _count;
+  export const first: _first;
+  export const filter: _filter;
+  export const concat: _concat;
+  export const isEmpty: _isEmpty;
+  export const forEach: _forEach;
+  export const takeWhile: _takeWhile;
+  export const skipWhile: _skipWhile;
+  export const enumerate: _enumerate;
 }
 
-export default List
+export default List;
--- index.js
+++ index.js
@@ -1,202 +1,201 @@
 export function isList(value) {
-  return typeof value === 'function' && Symbol.iterator in value
+  return typeof value === "function" && Symbol.iterator in value;
 }
 
 export function map(source, callback) {
   return callback
     ? from(function* () {
-        for (const value of source) yield callback(value)
+        for (const value of source) yield callback(value);
       })
-    : (anotherSource) => map(anotherSource, source)
+    : (anotherSource) => map(anotherSource, source);
 }
 
 export function chain(source, callback) {
   return callback
     ? from(function* () {
-        for (const value of source) yield* callback(value)
+        for (const value of source) yield* callback(value);
       })
-    : (anotherSource) => chain(anotherSource, source)
+    : (anotherSource) => chain(anotherSource, source);
 }
 
 export function filter(source, predicate) {
   return predicate
     ? from(function* () {
-        for (const value of source) if (predicate(value)) yield value
+        for (const value of source) if (predicate(value)) yield value;
       })
-    : (anotherSource) => filter(anotherSource, source)
+    : (anotherSource) => filter(anotherSource, source);
 }
 
 export function forEach(source, callback) {
-  if (callback) for (const value of source) callback(value)
-  else return (anotherSource) => forEach(anotherSource, source)
+  if (callback) for (const value of source) callback(value);
+  else return (anotherSource) => forEach(anotherSource, source);
 }
 
 export function of(...values) {
-  return from(values)
+  return from(values);
 }
 
 export function from(value) {
-  if (typeof value === 'function') {
-    value[Symbol.iterator] = value
+  if (typeof value === "function") return (value[Symbol.iterator] = value);
 
-    return value
-  }
-
-  if (!(Symbol.iterator in value)) value = Array.from(value)
+  if (!(Symbol.iterator in value)) value = Array.from(value);
 
   return from(function* () {
-    yield* value
-  })
+    yield* value;
+  });
 }
 
 export function fold(source, accumulator, reduce = accumulator) {
   if (isList(source)) {
-    let intermediate = accumulator
-    const iterableIterator = source()
+    let intermediate = accumulator;
+    const iterableIterator = source();
 
-    if (reduce === intermediate) intermediate = iterableIterator.next().value
+    if (reduce === intermediate) intermediate = iterableIterator.next().value;
 
     for (const value of iterableIterator)
-      intermediate = reduce(intermediate, value)
+      intermediate = reduce(intermediate, value);
 
-    return intermediate
-  } else return (anotherSource) => fold(anotherSource, source, accumulator)
+    return intermediate;
+  } else return (anotherSource) => fold(anotherSource, source, accumulator);
 }
 
 export function concat(source, other) {
   return other
     ? from(function* () {
-        yield* source
-        yield* other
+        yield* source;
+        yield* other;
       })
-    : (anotherSource) => concat(anotherSource, source)
+    : (anotherSource) => concat(anotherSource, source);
 }
 
 export function all(source, predicate) {
   if (predicate) {
-    for (const item of source) if (!predicate(item)) return false
+    for (const item of source) if (!predicate(item)) return false;
 
-    return true
-  } else return (anotherSource) => all(anotherSource, source)
+    return true;
+  } else return (anotherSource) => all(anotherSource, source);
 }
 
 export function any(source, predicate) {
   if (predicate) {
-    for (const item of source) if (predicate(item)) return true
+    for (const item of source) if (predicate(item)) return true;
 
-    return false
-  } else return (anotherSource) => any(anotherSource, source)
+    return false;
+  } else return (anotherSource) => any(anotherSource, source);
 }
 
 export function take(source, amount) {
   if (isList(source)) {
-    let _amount = amount
+    let _amount = amount;
 
-    return takeWhile(source, () => _amount-- > 0 || ((_amount = amount), false))
-  } else return (anotherSource) => take(anotherSource, source)
+    return takeWhile(
+      source,
+      () => _amount-- > 0 || ((_amount = amount), false),
+    );
+  } else return (anotherSource) => take(anotherSource, source);
 }
 
 export function takeWhile(source, predicate) {
   return predicate
     ? from(function* () {
         for (const value of source) {
-          if (predicate(value)) yield value
-          else return
+          if (predicate(value)) yield value;
+          else return;
         }
       })
-    : (anotherSource) => takeWhile(anotherSource, source)
+    : (anotherSource) => takeWhile(anotherSource, source);
 }
 
 export function skip(source, amount) {
   return isList(source)
     ? from(function* () {
-        let _amount = amount
+        let _amount = amount;
 
-        for (const value of source) _amount <= 0 ? yield value : _amount--
+        for (const value of source) _amount <= 0 ? yield value : _amount--;
       })
-    : (anotherSource) => skip(anotherSource, source)
+    : (anotherSource) => skip(anotherSource, source);
 }
 
 export function skipWhile(source, predicate) {
   return predicate
     ? from(function* () {
-        let skipping = true
+        let skipping = true;
 
         for (const value of source)
           skipping
             ? !(skipping = predicate(value)) && (yield value)
-            : yield value
+            : yield value;
       })
-    : (anotherSource) => skipWhile(anotherSource, source)
+    : (anotherSource) => skipWhile(anotherSource, source);
 }
 
 export function enumerate(source) {
   return from(function* () {
-    let index = 0
+    let index = 0;
 
-    for (const value of source) yield [value, index++]
-  })
+    for (const value of source) yield [value, index++];
+  });
 }
 
 export function sort(source, compare) {
   return compare
     ? from(function* () {
-        yield* Array.from(source).sort(compare)
+        yield* Array.from(source).sort(compare);
       })
-    : (anotherSource) => sort(anotherSource, source)
+    : (anotherSource) => sort(anotherSource, source);
 }
 
 export function count(source) {
-  return fold(source, 0, (amount) => amount + 1)
+  return fold(source, 0, (amount) => amount + 1);
 }
 
 export function scan(source, accumulator, reduce = accumulator) {
   return isList(source)
     ? from(function* () {
-        let intermediate = accumulator
-        const iterableIterator = source()
+        let intermediate = accumulator;
+        const iterableIterator = source();
 
         if (intermediate === reduce)
-          yield (intermediate = iterableIterator.next().value)
+          yield (intermediate = iterableIterator.next().value);
 
         for (const value of iterableIterator)
-          yield (intermediate = reduce(intermediate, value))
+          yield (intermediate = reduce(intermediate, value));
       })
-    : (anotherSource) => scan(anotherSource, source, accumulator)
+    : (anotherSource) => scan(anotherSource, source, accumulator);
 }
 
 export function first(source) {
-  return source().next().value
+  return source().next().value;
 }
 
 export function isEmpty(source) {
-  return Boolean(source().next().done)
+  return Boolean(source().next().done);
 }
 
 export function last(source) {
-  return fold(source, (_, value) => value)
+  return fold(source, (_, value) => value);
 }
 
 export function zip(source, other) {
   return other
     ? from(function* () {
-        const otherIterator = other()
+        const otherIterator = other();
 
         for (const item of source) {
-          const { done, value } = otherIterator.next()
+          const { done, value } = otherIterator.next();
 
-          if (done) return
+          if (done) return;
 
-          yield [item, value]
+          yield [item, value];
         }
       })
-    : (anotherSource) => zip(anotherSource, source)
+    : (anotherSource) => zip(anotherSource, source);
 }
 
 export function find(source, predicate) {
   if (predicate) {
-    for (const item of source) if (predicate(item)) return item
-  } else return (anotherSource) => find(anotherSource, source)
+    for (const item of source) if (predicate(item)) return item;
+  } else return (anotherSource) => find(anotherSource, source);
 }
 
 export default {
@@ -224,4 +223,4 @@ export default {
   takeWhile,
   skipWhile,
   enumerate,
-}
+};
